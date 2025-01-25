96daec3246b6d4d2e5d7c86aff6ec48bb694240e
vyokky <7678676@qq.com>
2025-01-25 22:48:39 +0800

    action

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/your-repo-name/', // Replace with your repository name
+  base: '/vyokky/personal', // Replace with your repository name
 });
